3868d99c7b17c1c86fab42314550d2f17b5b4f66
PhilipeF <philipesferreiraa@gmail.com>
2024-02-16 14:41:43 -0300

    Feat: exercicio_jquery

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-$(document).ready(()=> {
-    $
+$(document).ready(() => {
+    $('form').on('submit', (e) => {
+        e.preventDefault();
+        const novaTarefa = $('#novaTarefa').val();
+
+        if (novaTarefa.trim() !== '') {
+            $('ul').append(`<li>${novaTarefa}</li>`);
+        }
+
+        $('#novaTarefa').val('');
+
+        $(document).on('click', 'li', function () {
+            $(this).toggleClass('tarefaCompleta');
+        });
+    })
 })
